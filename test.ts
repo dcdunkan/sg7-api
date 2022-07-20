78ca3f3a367f4bde29f6f05bbe226bfe49d64398
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-21 00:24:59 +0530

    more testing

--- test.ts
+++ test.ts
@@ -3,6 +3,9 @@ import { serve } from "https://deno.land/std@0.148.0/http/mod.ts";
 serve(async () => {
   console.log(Deno.env.get("DENO_DIR"));
   console.log(Deno.cwd());
+  let f = 0;
+  for await (const _entry of Deno.readDir("./songs")) f++;
+  console.log(f);
   console.log(await Deno.readFile("./songs/eng/1.json"));
-  return new Response();
+  return new Response("Logged");
 });
